f26c9cbca81f471c9e796f1a705214f8bd31979b
Robert Watts <robw@wonderbly.com>
2019-12-29 23:13:34 +0000

    Fix homepage redirect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Redirect } from '@reach/router';
 
-const IndexPage = () => <Redirect to={`/squares-circles`} />;
+const IndexPage = () => <Redirect to={`/01-squares-circles`} />;
 
 export default IndexPage;
